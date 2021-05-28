1c2ecf3296a1f0c891283a720b527614f537f904
flafi <d.chalyi16@gmail.com>
2021-05-28 17:46:26 +0300

    Updated .mustache files

---
+++ src/main/java/com/rgr/project/webapp/repository/CustomerRepo.java
@@ -0,0 +1,14 @@
+package com.rgr.project.webapp.repository;
+
+import com.rgr.project.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CustomerRepo extends JpaRepository<CustomerEntity, Long> {
+    @Query("select cs from CustomerEntity cs where cs.customerPhone =?1")
+    CustomerEntity getCustomerById(String phone);
+
+    CustomerEntity findCustomerByCustomerId(Integer id);
+
+    CustomerEntity findCustomerByCustomerEmailAndCustomerPassword(String customerEmail, String customerPassword);
+}
